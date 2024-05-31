28efe7923e8ddd14b2c66605a08f3127f6d093f8
André Torres <andretorresengm@hotmail.com>
2024-05-31 23:48:13 +0100

    projeto finalizado

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export const Header = () => {
+  return(
+    <header className="header">
+    <h1 className="h1">GitFind</h1>
+    </header>
+  );
+};
---
+++ src/components/itemList/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.css';
+
+export const ItemList = ({title, description}) => {
+  return (
+    <>
+    <div className="item-list">
+    <strong>{title}</strong>
+    <p>{description}</p>
+    <hr/>
+    </div>
+    </>
+  );
+  
+ 
+}
